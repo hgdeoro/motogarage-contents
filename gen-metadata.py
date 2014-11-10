0b784d62fecddb193f5e10a32b360d9bb1531199
Horacio G. de Oro <hgdeoro@gmail.com>
2014-11-09 22:27:52 -0300

    Agrega foro

--- gen-metadata.py
+++ gen-metadata.py
@@ -30,6 +30,29 @@ def main():
 				],
 				'image_url': '',
 			},
+			{
+				'country': 'ar',
+				'name': 'Los Cuises - Moteros Cordobeses',
+				'url': 'moteroscordobeses.com.ar',
+				'vehicles': [
+					{
+						'code': 'moto.honda.nx4',
+						'name': 'Honda Falcon NX4',
+						'spec': [
+							{'name': 'Aceite - Tipo',				'value': 'Mineral - SAE 20W-50',},
+							{'name': 'Aceite - Capacidad maxima',	'value': '2,2 litros',},
+							{'name': 'Aceite - Marcas',				'value': 'Mobil Supermoto 4T Multigrado SAE 20W50 API-SF, Castrol Actevo 20W50',},
+						],
+						'mpt': [
+							{'code': 'cambio-aceite',			'name': 'Cambio de aceite',				'km': 3000,},
+							{'code': 'cambio-filtro-aceite',	'name': 'Cambio de filtro de aceite',	'km': 3000,},
+						],
+					},
+				],
+				'mobile_msg': [
+				],
+				'image_url': '',
+			},
 		]
 	}
 	
